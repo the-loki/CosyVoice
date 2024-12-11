f2345947f7e0063ab847c221debb04f0d19b4d5f
loki7 <polaris404@foxmail.com>
2024-12-11 14:57:43 +0800

    feat: use single model to build docker image.

--- download_model.py
+++ download_model.py
@@ -3,5 +3,4 @@ from modelscope import snapshot_download
 
 model_name = os.environ.get('TARGET_COSY_MODEL')
 
-snapshot_download('iic/{}'.format(model_name), local_dir='pretrained_models/{}'.format(model_name))
-snapshot_download('iic/CosyVoice-ttsfrd', local_dir='pretrained_models/CosyVoice-ttsfrd')
+snapshot_download('iic/{}'.format(model_name))
